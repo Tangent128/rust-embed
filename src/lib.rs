dfd26b286340d2e0bef18661ac14560867180ee1
Tangent 128 <Tangent128@gmail.com>
2019-01-11 00:07:41 -0500

    Optimization: don't box iterators in release mode

--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,20 @@ pub mod utils;
 
 pub trait RustEmbed {
   fn get(&self, file_path: &str) -> Option<std::borrow::Cow<'static, [u8]>>;
-  fn iter(&self) -> Box<dyn Iterator<Item = std::borrow::Cow<'static, str>>>;
+  fn iter(&self) -> Filenames;
+}
+
+pub enum Filenames {
+  Embedded(std::slice::Iter<'static, &'static str>),
+  Dynamic(Box<dyn Iterator<Item = std::borrow::Cow<'static, str>>>),
+}
+
+impl Iterator for Filenames {
+  type Item = std::borrow::Cow<'static, str>;
+  fn next(&mut self) -> Option<Self::Item> {
+    match self {
+      Filenames::Embedded(names) => names.next().map(|x| std::borrow::Cow::from(*x)),
+      Filenames::Dynamic(boxed) => boxed.next(),
+    }
+  }
 }
